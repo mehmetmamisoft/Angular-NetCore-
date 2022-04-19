8b67fbad227be373e383f06001c7e1eb9b01fe35
mehmet seydal <mehemet01@hotmail.com>
2022-04-19 23:17:53 +0300

    18 nisan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //index.html'den geliyo
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'Mehmet Seydal';
+
 }
