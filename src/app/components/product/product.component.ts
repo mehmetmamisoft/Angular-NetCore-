4210dc938822bfc8094f140136e0424520e9a174
mehmet seydal <mehemet01@hotmail.com>
2022-04-26 01:45:38 +0300

    26 nisan api

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +9,28 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
+  //gerçek data burasu
   products: Product[] = [];
+  apiUrl = 'https://localhost:44335/api/products/getall';
 
-  constructor() {}
+  //şimdillik gerek kalmadu
+  //productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // };
 
-  ngOnInit(): void {}
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
