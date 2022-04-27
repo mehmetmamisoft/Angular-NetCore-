413f709b1366cf473e7870e149dd9d81318e4ff4
mehmet seydal <mehemet01@hotmail.com>
2022-04-27 05:25:03 +0300

    27 nisan commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -18,21 +19,47 @@ export class ProductComponent implements OnInit {
   //   success:true
   // };
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+
+    ) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    //this.getProducts(); ilk hali bu
+    //params parametre bursau route göre hareket ediyi
+    //parametre varsa categoryId yoksa getproducts gelcek!
+    this.activatedRoute.params.subscribe(params=>{
+   if (params["categoryId"]) {
+      this.getProductsByCategory(params["categoryId"])
+   }
+   else{
+     this.getProducts();
+   }
+
+    })
   }
 
   getProducts() {
     //console.log("api request başladu");
-    this.productService.getProducts().subscribe(response=>{
+    this.productService.getProducts()
+    .subscribe(response=>{
       this.products=response.data;
       this.dataLoaded=true;
     //console.log("api request bitti");
 
     });
     //console.log("Method Bittu");
+
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId)
+    .subscribe(response=>{
+      this.products=response.data;
+      this.dataLoaded=true;
+
+    });
 
   }
 }
